d4ca452b1bb5cfd1c48c39d83f3a47969f2cc855
kotarochan456 <lanceangelescobanas@gmail.com>
2026-03-09 17:29:33 +0800

    Refactor ProfileScreenActivity and handle password data
    Refactored ProfileScreenActivity to extend Activity instead of AppCompatActivity and added handling for incoming password data.

--- ProfileScreenActivity.kt
+++ ProfileScreenActivity.kt
@@ -1,24 +1,29 @@
 package com.example.hirepath
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.util.Log
 
 
-class ProfileScreenActivity : AppCompatActivity() {
+
+class ProfileScreenActivity : Activity() {
 
     // Variable following camelCase
     private lateinit var buttonBack: Button
     private lateinit var textviewUsernameValue: TextView
+    private lateinit var textviewPasswordValue: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_screen)
 
+        textviewPasswordValue = findViewById(R.id.textviewPasswordValue)
+
         initializeViews()
         setupClickListeners()
         displayUserData()
+        handleIncomingData()
     }
 
     private fun initializeViews() {
@@ -38,6 +43,13 @@ class ProfileScreenActivity : AppCompatActivity() {
         intent.getStringExtra("username")?.let { username ->
             Log.d("App", "Profile received username: $username")
             textviewUsernameValue.text = username
+        }
+    }
+
+    private fun handleIncomingData() {
+        intent.getStringExtra("password")?.let { password ->
+            // Use .text = password, NOT textviewPasswordValue = password
+            textviewPasswordValue.text = password
         }
     }
 
